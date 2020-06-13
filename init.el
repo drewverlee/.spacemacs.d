abe430436547124e1ab7e0769300868c7e23832c
drewverlee <drew.verlee@gmail.com>
2020-06-13 13:22:40 -0500

    this extra enforces structural editing!

--- init.el
+++ init.el
@@ -545,6 +545,8 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place you code here."
 
+  (spacemacs/toggle-evil-safe-lisp-structural-editing-on-register-hook-clojure-mode)
+
   ;;
   ;; Magit - forge configuration
   ;;
